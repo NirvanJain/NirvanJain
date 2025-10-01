a73bd892a3665171a50c78e098ecd76dd4f7b7c5
Nirvan Jain <151359367+NirvanJain@users.noreply.github.com>
2025-10-01 16:58:53 +0530

    Add files via upload

---
+++ nj_java.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class PrintName {
+    public static void main(String[] args) {
+        // This line prints the string to the console
+        System.out.println("Nirvan Jain");
+    }
+}
